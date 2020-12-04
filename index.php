7fd70e1fdcdc196f8ad220e231b9b668210cc691
ySoHai <39169678+ySoHai@users.noreply.github.com>
2020-12-03 16:10:05 -0800

    3

--- index.php
+++ index.php
@@ -1,24 +1,52 @@
-<!DOCTYPE html>
+<?php
 
+  require "header.php";
 
-<head>
-  <title>Drag and Drop File Upload</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta charset="utf-8">
-  <link rel="shortcut icon" href="/assets/favicon.ico">
-  <link rel="stylesheet" href="./src/main.css">
-</head>
+?>
 
-<body>
-  <form action="src/uploads.php" method="POST" enctype="multipart/form-data">
-  <div class="drop-zone">
-      <span class="drop-zone__prompt">Drop file here or click to upload</span>
-      <input type="file" name="fileToUpload" id="fileToUpload" class="drop-zone__input">
+<main>
+  <section class="cases-links">
+    <div class="wrapper">
+      <h2>Files</h2>
+      <div class="gallery-container">
+
+        <?php
+          include_once "includes/dbh.inc.php";
+
+          $sql = "SELECT * FROM gallery ORDER BY orderGallery DESC;";
+          $stmt = mysqli_stmt_init($conn);
+          if (!mysqli_stmt_prepare($stmt, $sql)) {
+            echo "SQL statement failed!";
+          }
+          else {
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+            while ($row = mysqli_fetch_assoc($result)) {
+              echo '<a href="#">
+                <div style="background-image: url(img/gallery/' . $row["imgFullNameGallery"] . ');"></div>
+                <h3>' . $row["titleGallery"] . '</h3>
+                <p>' . $row["descGallery"] . '</p>
+              </a>';
+            }
+          }
+        ?>
+
       </div>
-      <button type="submit" name="submit">Click Me To Upload</button>
-  </form>
 
+      <?php
+          echo'<div class="gallery-upload">
+            <form class="form-signup" action="includes/uploads.php" method="post" enctype="multipart/form-data">
+              <input type="file" name="file">
+              <button type="submit" name="submit">UPLOAD</button>
+            </form>
+          </div>';
+      ?>
 
+    </div>
+  </section>
+</main>
 
-</body>
-<script src="./src/main.js"></script>
+<?php
+
+  require_once "footer.php";
+?>
