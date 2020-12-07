b89e85a5c81be0a36235ceaf82df97e1d7045038
ySoHai <39169678+ySoHai@users.noreply.github.com>
2020-12-06 17:20:14 -0800

    ?????????

--- index.php
+++ index.php
@@ -11,10 +11,23 @@
       <div class="gallery-container">
 
         <?php
-        foreach (new DirectoryIterator('../uploads') as $fileInfo) {
-            if($fileInfo->isDot()) continue;
-            echo $fileInfo->getFilename() . "<br>\n";
-        }
+        // Create recursive dir iterator which skips dot folders
+        $dir = new RecursiveDirectoryIterator('./uploads', FilesystemIterator::SKIP_DOTS);
+
+        // Flatten the recursive iterator, folders come before their files
+        $it  = new RecursiveIteratorIterator($dir, RecursiveIteratorIterator::SELF_FIRST);
+
+        // Maximum depth is 1 level deeper than the base folder
+        $it->setMaxDepth(1);
+
+        // Basic loop displaying different messages based on file or folder
+        foreach ($it as $fileinfo) {
+            if ($fileinfo->isDir()) {
+                printf("Folder - %s\n", $fileinfo->getFilename());
+            } elseif ($fileinfo->isFile()) {
+                printf("File From %s - %s\n", $it->getSubPath(), $fileinfo->getFilename());
+            }
+}
         /*while ($row = mysqli_fetch_assoc($result)) {
           echo '<a href="#">
             <div style="background-image: url(img/gallery/' . $row["imgFullNameGallery"] . ');"></div>
