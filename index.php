b5e3ed5287ab1aadde0b4be5e0c72ce2ac8ad7ec
ySoHai <39169678+ySoHai@users.noreply.github.com>
2020-12-06 16:53:49 -0800

    test index

--- index.php
+++ index.php
@@ -11,24 +11,21 @@
       <div class="gallery-container">
 
         <?php
-          include_once "includes/dbh.inc.php";
-
-          $sql = "SELECT * FROM gallery ORDER BY orderGallery DESC;";
-          $stmt = mysqli_stmt_init($conn);
-          if (!mysqli_stmt_prepare($stmt, $sql)) {
-            echo "SQL statement failed!";
-          }
-          else {
-            mysqli_stmt_execute($stmt);
-            $result = mysqli_stmt_get_result($stmt);
-            while ($row = mysqli_fetch_assoc($result)) {
-              echo '<a href="#">
-                <div style="background-image: url(img/gallery/' . $row["imgFullNameGallery"] . ');"></div>
-                <h3>' . $row["titleGallery"] . '</h3>
-                <p>' . $row["descGallery"] . '</p>
-              </a>';
-            }
-          }
+        $dir = '../uploads/';
+        $scan = scandir($dir);
+        foreach($scan as $file) {
+           if (!is_dir($dir.$file)) {
+              echo $file.'\n';
+           }
+        }
+        /*while ($row = mysqli_fetch_assoc($result)) {
+          echo '<a href="#">
+            <div style="background-image: url(img/gallery/' . $row["imgFullNameGallery"] . ');"></div>
+            <h3>' . $row["titleGallery"] . '</h3>
+            <p>' . $row["descGallery"] . '</p>
+          </a>';
+        }
+        */
         ?>
 
       </div>
