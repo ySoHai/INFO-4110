a915602dbea1fbd7e4f8c24bf1788ef4cb0811ee
ySoHai <39169678+ySoHai@users.noreply.github.com>
2020-12-08 00:24:22 -0800

    up

---
+++ includes/files.php
@@ -0,0 +1,7 @@
+<?php
+// Create recursive dir iterator which skips dot folders
+$dir = new RecursiveDirectoryIterator('./uploads', FilesystemIterator::SKIP_DOTS);
+// Flatten the recursive iterator, folders come before their files
+$it  = new RecursiveIteratorIterator($dir, RecursiveIteratorIterator::SELF_FIRST);
+// Maximum depth is 1 level deeper than the base folder
+$it->setMaxDepth(1);
--- index.php
+++ index.php
@@ -11,21 +11,17 @@
       <div class="gallery-container">
 
         <?php
-        // Create recursive dir iterator which skips dot folders
-        $dir = new RecursiveDirectoryIterator('./uploads', FilesystemIterator::SKIP_DOTS);
-        // Flatten the recursive iterator, folders come before their files
-        $it  = new RecursiveIteratorIterator($dir, RecursiveIteratorIterator::SELF_FIRST);
-        // Maximum depth is 1 level deeper than the base folder
-        $it->setMaxDepth(1);
+        require "includes/files.php";
         // Basic loop displaying different messages based on file or folder
         foreach ($it as $fileinfo) {
           if ($fileinfo->isFile()) {
-              echo '<a href="#">
-              <div style="background-image: url(uploads/' .$it->getSubPath().'/'. $fileinfo->getFilename() . ');"></div>
+              echo '<div class="file-container">
+              <div style="background-image: url(uploads/' . $it->getSubPath().'/'. $fileinfo->getFilename() . ');"></div>
               <h3> Folder: ' . $it->getSubPath() . '</h3>
-              <p>' . $fileinfo->getFilename() . '</p>
-              </a>';
-            }
+              <p>' . $fileinfo->getFilename() . 'Size:'.$fileinfo->getSize().'</p>
+              <a href="includes/download.php?file='. $it->getSubPath().'/'. $fileinfo->getFilename() .'"></a>
+              </div>';
+            }// get size
         }
 
       ?>
