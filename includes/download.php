ca9ee05617c1ed76a50eabf13f670ca9dc5c64c2
ySoHai <39169678+ySoHai@users.noreply.github.com>
2020-12-08 13:11:49 -0800

    update

--- includes/download.php
+++ includes/download.php
@@ -1,23 +1,36 @@
 <?php
 setlocale(LC_ALL,'en_US.UTF-8');
 
-$fileName = basename($_GET['file']);
-$fileExt = pathinfo($fileName, PATHINFO_EXTENSION);
-$targetDir = '../uploads/'.$fileExt.'/';
-$filePath = $targetDir.$fileName;
+if(isset($_REQUEST["file"])){
+    // Get parameters
+    $file = urldecode($_REQUEST["file"]); // Decode URL-encoded string
 
-if(file_exists($filePath)){ // file does not exist
-  header('Content-Description: File Transfer');
-  header('Content-Type: application/octet-stream');
-  header('Content-Disposition: attachment; filename="'.basename($fileName).'"');
-  header('Expires: 0');
-  header('Cache-Control: must-revalidate');
-  header('Pragma: public');
-  header('Content-Length: ' . filesize($fileName));
-  echo $filePath;
-  //readfile($filePath);
-} else {
-  die('file not found');
+    /* Test whether the file name contains illegal characters
+    such as "../" using the regular expression */
+    if(preg_match('/^[^.][-a-z0-9_.]+[a-z]$/i', $file)){
+        $fileExt = pathinfo($file, PATHINFO_EXTENSION);
+        $filepath = '../uploads/'.$fileExt.'/'.$file.;
+
+        // Process download
+        if(file_exists($filepath)) {
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename="'.basename($filepath).'"');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($filepath));
+            flush(); // Flush system output buffer
+            readfile($filepath);
+            die();
+        } else {
+            http_response_code(404);
+	        die();
+        }
+    } else {
+        die("Invalid file name!");
+    }
 }
+
 
 ?>
--- includes/filecontainer.php
+++ includes/filecontainer.php
@@ -13,7 +13,7 @@ foreach ($it as $fileinfo) {
       <h3> Folder: ' . $it->getSubPath() . '</h3>
       <p>' . $fileinfo->getFilename() . '</p>
       <p>Size: ' . $fileinfo->getSize() . '</p>
-      <a href="includes/download.php?file='. $fileinfo->getFilename() .'"></a>
+      <a href="includes/download.php?file='. urlencode($fileinfo->getFilename()) .'"></a>
       </a>';
     }
 }
