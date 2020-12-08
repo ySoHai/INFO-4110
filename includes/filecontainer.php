1b49da65d15ef50b33294ade0f968273c90c7e9a
ySoHai <39169678+ySoHai@users.noreply.github.com>
2020-12-08 15:43:50 -0800

    Update filecontainer.php

--- includes/filecontainer.php
+++ includes/filecontainer.php
@@ -5,20 +5,29 @@ $dir = new RecursiveDirectoryIterator('./uploads', FilesystemIterator::SKIP_DOTS
 $it  = new RecursiveIteratorIterator($dir, RecursiveIteratorIterator::SELF_FIRST);
 // Maximum depth is 1 level deeper than the base folder
 $it->setMaxDepth(1);
+
+echo '<table>
+      <tr>
+      <th>Folder</th>
+      <th>File Name</th>
+      <th>Size</th>
+      <th>Dowload Link</th>
+      </tr>';
 // Basic loop displaying different messages based on file or folder
 foreach ($it as $fileinfo) {
-
-  if ($fileinfo->isFile()) {
-      echo '<a>
-      <div style="background-image: url(uploads/' . $it->getSubPath() .'/'. $fileinfo->getFilename() . ');"></div>
-      <h3> Folder: ' . $it->getSubPath() . '</h3>
-      <p>' . $fileinfo->getFilename() . '</p>
-      <p>Size: ' . $fileinfo->getSize() . '</p>
-      <a href="includes/download.php?file='. urlencode($fileinfo->getFilename()) .'"></a>
-      </a>';
-    }
+  if ($fileinfo->isDir()){
+    echo '<tr>
+          <td>'.$fileinfo->getFilename.'</td>';
+    } elseif ($fileinfo->isFile()) {
+        echo '<td>'.$fileinfo->getFilename() . '</td>
+              <td>' . $fileinfo->getSize() . '</td>
+              <td><a href="includes/download.php?file='. urlencode($fileinfo->getFilename()) .'">Download</a></td>
+              </tr>';
+      }
 
 }
+
+echo '</table>';
 
 /* OLD file conteiner
   echo '<a>
