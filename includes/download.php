6adfbc9cd78e1cd109d92eb9ae77cf5cc8fb12a7
ySoHai <39169678+ySoHai@users.noreply.github.com>
2020-12-08 01:26:25 -0800

    Update download.php

--- includes/download.php
+++ includes/download.php
@@ -1,19 +1,21 @@
 <?
 $file = basename($_GET['file']);
 $fileExt = explode('.', $file)
+echo $fileExt;
 $targetDir = '../uploads/';
 $filePath = $targetDir.$fileExt.'/'.$file;
 
-if(!file_exists($filePath)){ // file does not exist
-    die('file not found');
+if(file_exists($filePath)){ // file does not exist
+  header('Content-Description: File Transfer');
+  header('Content-Type: application/octet-stream');
+  header('Content-Disposition: attachment; filename="'.basename($filePath).'"');
+  header('Expires: 0');
+  header('Cache-Control: must-revalidate');
+  header('Pragma: public');
+  header('Content-Length: ' . filesize($filePath));
+  readfile($filePath);
 } else {
-    header("Cache-Control: public");
-    header("Content-Description: File Transfer");
-    header("Content-Disposition: attachment; filename=$file");
-    header('Content-Type: ' . $fileExt);
-    header("Content-Transfer-Encoding: binary");
 
-    // read the file from disk
-    readfile($file);
+  die('file not found');
 }
 ?>
