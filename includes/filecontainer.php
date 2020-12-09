224d343864bb41c2b54a1769644a6bbb96004b0e
matheus-onshape <moliveira@ptc.com>
2020-12-08 23:13:31 -0500

    time to test

--- includes/filecontainer.php
+++ includes/filecontainer.php
@@ -52,6 +52,27 @@ function dir_is_empty($path)
     return $empty;
 }
 
+
+$mainDataArr = array();
+foreach ($it as $fileinfo) {
+  if ($fileinfo->isDir()) {
+    $$fileinfo->getFilename() = array();
+    $$fileinfo->getFilename()[0] = array() //fileName
+    $$fileinfo->getFilename()[1] = array() //fileSize
+    $$fileinfo->getFilename()[2] = array() //fileName
+    $mainDataArr.push($$fileinfo->getFilename())
+  } elseif ($fileinfo->isFile()) {
+    //populate arrays
+    $withoutExt = preg_replace('/\\.[^.\\s]{2,6}$/', '', $fileInfo->getFilename());
+    $mainDataArr[$$withoutExt][0] = $fileinfo->getFilename()
+    $mainDataArr[$$withoutExt][1] = formatSizeUnits($fileinfo->getSize())
+    $mainDataArr[$$withoutExt][2] = urlencode($fileinfo->getFilename())
+    //array[0] = fileName
+    //array[1] = fileSize
+    //array[2] = link
+  }
+}
+
 echo '<table>
       <tr>
       <th>Folder</th>
@@ -59,42 +80,84 @@ echo '<table>
       <th>Size</th>
       <th>Dowload Link</th>
       </tr>';
-// Basic loop displaying different messages based on file or folder
-  foreach ($it as $fileinfo) {
-      if ($fileinfo->isDir()) {
-        echo '<tr>';
-        if (dir_is_empty('./uploads/' . $fileinfo->getFilename() . '/')) {
-          echo '<td><u><b>'. strtoupper($fileinfo->getFilename()).'</b></u></td>
-                </tr>
-                <tr>
-                <td></td>
-                <td>No Files</td>
-                </tr>';
-        }else {
-          echo '<td><u><b>'. strtoupper($fileinfo->getFilename()).'</b></u></td>
+foreach ($mainDataArr as &value){
+  //mainDataArr[n] = fileType[]
+  if (empty($value)){
+    $foldername = preg_replace('/\\.[^.\\s]{2,6}$/', '', $value[0]);
+    $fileName = $value[0]
+    $fileSize = $value[1]
+    $fileDownload =$value[2]
+    echo '<td><u><b>'. strtoupper($foldername).'</b></u></td>
+          </tr>
+          <tr>
+          <td></td>
+          <td>No Files</td>
           </tr>';
-        }
+  } else {
+    echo '<td><u><b>'. strtoupper($foldername).'</b></u></td>
+          </tr>'
+          '<tr>
+          <td></td>
+          <td>';
+    if (in_array($foldername, $supported_image)) {
+      echo '<img src="../uploads/' . strtolower($foldername) . '/' .$fileName.'" width="35" height="35">';
+    }
 
-      }elseif ($fileinfo->isFile()) {
-            echo '<tr>
-                  <td></td>
-                  <td>';
-            if (in_array($it->getSubPath(), $supported_image)) {
-              echo '<img src="../uploads/' . $it->getSubPath() . '/' .$fileinfo->getFilename().'" width="35" height="35">';
-            }
-
-            echo  $fileinfo->getFilename() . '</td>
-                  <td style="text-align: center; vertical-align: middle;">' . formatSizeUnits($fileinfo->getSize()) . '</td>
-                  <td style="text-align: center; vertical-align: middle;"><a href="includes/download.php?file='. urlencode($fileinfo->getFilename()) .'">Download</a></td>
-                  </tr>';
-          }else {
-            echo '<tr>
-                  <td>Folder Empty</td>
-                  </tr>';
-          }
+    echo  $fileName . '</td>
+          <td style="text-align: center; vertical-align: middle;">' . formatSizeUnits($fileSize) . '</td>
+          <td style="text-align: center; vertical-align: middle;"><a href="includes/download.php?file='. $fileDownload .'">Download</a></td>
+          </tr>';
+  } else {
+    echo '<tr>
+          <td>Folder Empty</td>
+          </tr>';
   }
+}
 
-echo '</table>';
+
+// echo '<table>
+//       <tr>
+//       <th>Folder</th>
+//       <th>File Name</th>
+//       <th>Size</th>
+//       <th>Dowload Link</th>
+//       </tr>';
+// // Basic loop displaying different messages based on file or folder
+//   foreach ($it as $fileinfo) {
+//       if ($fileinfo->isDir()) {
+//         echo '<tr>';
+//         if (dir_is_empty('./uploads/' . $fileinfo->getFilename() . '/')) {
+//           echo '<td><u><b>'. strtoupper($fileinfo->getFilename()).'</b></u></td>
+//                 </tr>
+//                 <tr>
+//                 <td></td>
+//                 <td>No Files</td>
+//                 </tr>';
+//         }else {
+//           echo '<td><u><b>'. strtoupper($fileinfo->getFilename()).'</b></u></td>
+//           </tr>';
+//         }
+
+//       }elseif ($fileinfo->isFile()) {
+//             echo '<tr>
+//                   <td></td>
+//                   <td>';
+//             if (in_array($it->getSubPath(), $supported_image)) {
+//               echo '<img src="../uploads/' . $it->getSubPath() . '/' .$fileinfo->getFilename().'" width="35" height="35">';
+//             }
+
+//             echo  $fileinfo->getFilename() . '</td>
+//                   <td style="text-align: center; vertical-align: middle;">' . formatSizeUnits($fileinfo->getSize()) . '</td>
+//                   <td style="text-align: center; vertical-align: middle;"><a href="includes/download.php?file='. urlencode($fileinfo->getFilename()) .'">Download</a></td>
+//                   </tr>';
+//           }else {
+//             echo '<tr>
+//                   <td>Folder Empty</td>
+//                   </tr>';
+//           }
+//   }
+
+// echo '</table>';
 
 /* OLD file conteiner
   echo '<a>
